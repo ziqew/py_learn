a79a3d6ea93aebb878beacdd7ac9f086166f4acf
ziqew <gongwenwei@gmail.com>
2014-03-06 16:53:43 +0800

    delete

--- twitter_search.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'ziqew'
-import const_val
-from twitter import *
-t = Twitter(auth=OAuth(const_val.TT_OAUTH_TOKEN, const_val.TT_OAUTH_SECRET,
-                       const_val.TT_CONSUMER_KEY, const_val.TT_CONSUMER_SECRET))
-
-trends = t.trends.place(_id="1")
-print trends
